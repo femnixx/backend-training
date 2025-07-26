8bde6fa7827e1653d318f70de5935aaa9bd5bee5
femnix <femnitality@gmail.com>
2025-07-26 16:38:00 +0700

    Made button back to landingpage

--- frontend/pages/LoginPage.tsx
+++ frontend/pages/LoginPage.tsx
@@ -1,10 +1,17 @@
+import { Outlet, Link } from "react-router-dom";
+
 const LoginPage = () => {
   return (
     <>
       <div className=" bg-gray-400 ms-4">
         <div className="bg-white flex flex-col">
-          <p className="text-center mt-5">Welcome back</p>
-          <div className="gap-y-3">
+          <div className="flex justify-center">
+            <Link to="/" className="text-start mt-5 absolute left-5">
+              Click me to come back
+            </Link>
+            <p className="text-center mt-5">Welcome back</p>
+          </div>
+          <div className="gap-y-3 mt-5">
             <p>Email</p>
             <input
               type="email"
