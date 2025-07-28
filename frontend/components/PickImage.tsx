ae5edc2ae9caebaa74d2b872c4d6052164ca7235
femnix <femnitality@gmail.com>
2025-07-28 08:57:00 +0700

    Fixed the pickImage component again

--- frontend/components/PickImage.tsx
+++ frontend/components/PickImage.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { supabase } from "../../backend/services/Supabase.ts";
 
 const PickImage = () => {
-  <>
+  return (
     <div className="border-1 w-fit px-2 hover:cursor-pointer">Pick image</div>
-  </>;
+  );
 };
 
 export default PickImage;
